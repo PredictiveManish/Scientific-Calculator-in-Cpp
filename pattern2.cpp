d33971362de4a7072f087c1ade080881258a9066
Manish Tiwari <manisht0914@gmail.com>
2024-10-16 01:54:32 +0000

    update

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,2 +1,16 @@
 // Inverted half pyramid
 #include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+
+    }
+    return 0;
+    
+}
